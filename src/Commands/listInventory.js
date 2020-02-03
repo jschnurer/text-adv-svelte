926d04e133b55079a4bfe5bbdecfad6e3fe028c2
Justin Schnurer <JSchnurer@slb.com>
2020-02-03 16:25:12 -0500

    sorted inventory list

--- src/Commands/listInventory.js
+++ src/Commands/listInventory.js
@@ -2,7 +2,7 @@ export default function listInventory() {
   this.text = "You check what you're carrying...";
   if (this.inventory.length) {
     this.write("You have:");
-    this.inventory.forEach(x => this.write(x.slug));
+    this.inventory.sort((a,b) => a.slug < b.slug ? -1 : 1).forEach(x => this.write(x.slug));
     this.write("...");
   } else {
     this.write("You don't have anything.");
