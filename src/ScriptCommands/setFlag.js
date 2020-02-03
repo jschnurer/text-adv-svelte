f968ef8fa56c77d83714b1a140da9b6d71365005
Justin Schnurer <JSchnurer@slb.com>
2020-02-03 12:04:00 -0500

    allowed global flags

--- src/ScriptCommands/setFlag.js
+++ src/ScriptCommands/setFlag.js
@@ -1,5 +1,8 @@
 /// If the name begins with "G.", a global flag will be set instead.
 export default function setFlag(name) {
-  let lVars = this.getLocalVars();
-  lVars[name] = true;
+  if (name.startsWith('G.')) {
+    this.globalVars[name] = true;
+  } else {
+    this.getLocalVars()[name] = true;
+  }
 }
