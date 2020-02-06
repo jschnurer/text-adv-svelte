1c492cd66da114ab3a24c7eb19da80861ce859e5
Justin Schnurer <JSchnurer@slb.com>
2020-02-06 09:43:40 -0500

    added rooms

--- src/Rooms/room-index.js
+++ src/Rooms/room-index.js
@@ -1,9 +1,19 @@
-export {default as f_brokenBridge} from "./Forest/broken-bridge.json";
-export {default as f_quietFork} from "./Forest/quiet-fork.json";
-export {default as f_westClearing} from "./Forest/west-clearing.json";
-export {default as f_riverBank} from "./Forest/river-bank.json";
-export {default as f_Hill} from "./Forest/hill.json";
+// Tutorial
 export {default as t_Center} from "./Tutorial/center.json";
 export {default as t_West} from "./Tutorial/west.json";
 export {default as t_North} from "./Tutorial/north.json";
 export {default as t_East} from "./Tutorial/east.json";
+
+// Forest
+export {default as f_brokenBridge} from "./Forest/broken-bridge.json";
+export {default as f_quietFork} from "./Forest/quiet-fork.json";
+export {default as f_westClearing} from "./Forest/west-clearing.json";
+export {default as f_riverBank} from "./Forest/river-bank.json";
+export {default as f_hill} from "./Forest/hill.json";
+export {default as f_lonelyPath} from "./Forest/lonely-path.json";
+export {default as f_bendInThePath} from "./Forest/bend-in-the-path.json";
+export {default as f_abruptTurn} from "./Forest/abrupt-turn.json";
+export {default as f_ruinsEntrance} from "./Forest/ruins-entrance.json";
+
+// Ruins
+export {default as r_forestEntrance} from "./Ruins/forest-entrance.json";
