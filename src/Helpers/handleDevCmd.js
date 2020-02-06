2d2a1088154e6a5a71c3256e78550a93e71a1136
Justin Schnurer <JSchnurer@slb.com>
2020-02-06 09:25:52 -0500

    wrote dev command output to screen

--- src/Helpers/handleDevCmd.js
+++ src/Helpers/handleDevCmd.js
@@ -2,21 +2,21 @@ export default function handleDevCmd(str) {
   if (str.startsWith('setFlag')) {
     let flagName = str.split('|')[1];
     this.setFlag(flagName);
-    console.log(`Set ${flagName} to true.`);
+    this.write(`~Set ${flagName} to true.~`);
   } else if (str.startsWith('unsetFlag')) {
     let flagName = str.split('|')[1];
     this.unsetFlag(flagName);
-    console.log(`Set ${flagName} to false.`);
+    this.write(`~Set ${flagName} to false.~`);
   } else if (str.startsWith('addItem')) {
     let itemId = str.split('|')[1];
     this.addItem(itemId);
-    console.log(`Add ${itemId} to inventory.`);
+    this.write(`~Added ${itemId} to inventory.~`);
   } else if (str.startsWith('removeItem')) {
     let itemId = str.split('|')[1];
     this.removeItem(itemId);
-    console.log(`Removed ${itemId} from inventory.`);
+    this.write(`~Removed ${itemId} from inventory.~`);
   } else if (str === 'saveGame') {
     this.saveGame();
-    console.log('Game saved.');
+    this.write('~Game Saved~');
   }
 }
