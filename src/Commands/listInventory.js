6ac6554ef8c81236a7af87b97bb1d6169c5df570
Justin Schnurer <justinschnurer@gmail.com>
2020-02-08 15:51:16 -0500

    changed inventory to not clear screen

--- src/Commands/listInventory.js
+++ src/Commands/listInventory.js
@@ -1,5 +1,5 @@
 export default function listInventory() {
-  this.text = "You check what you're carrying...";
+  this.text += "\nYou check what you're carrying...";
   if (this.inventory.length) {
     this.write("You have:");
     this.inventory.sort((a,b) => a.slug < b.slug ? -1 : 1).forEach(x => this.write(x.slug));
