339523d4f730b0429f110122234fa21c94c1cbf9
Justin Schnurer <JSchnurer@slb.com>
2020-02-10 16:59:03 -0500

    added empty rooms in swamp

--- src/Rooms/room-index.js
+++ src/Rooms/room-index.js
@@ -1,31 +1,43 @@
 // Tutorial
-export {default as t_Center} from "./Tutorial/center.json";
-export {default as t_learning} from "./Tutorial/learning.json";
-export {default as t_computer} from "./Tutorial/computer-room.json";
-export {default as t_teleporter} from "./Tutorial/teleporter.json";
-export {default as t_mechanical} from "./Tutorial/mechanism-room.json";
-export {default as t_northAccess} from "./Tutorial/north-access.json";
+export { default as t_Center } from "./Tutorial/center.json";
+export { default as t_learning } from "./Tutorial/learning.json";
+export { default as t_computer } from "./Tutorial/computer-room.json";
+export { default as t_teleporter } from "./Tutorial/teleporter.json";
+export { default as t_mechanical } from "./Tutorial/mechanism-room.json";
+export { default as t_northAccess } from "./Tutorial/north-access.json";
 
 // Forest
-export {default as f_brokenBridge} from "./Forest/broken-bridge.json";
-export {default as f_quietFork} from "./Forest/quiet-fork.json";
-export {default as f_westClearing} from "./Forest/west-clearing.json";
-export {default as f_riverBank} from "./Forest/river-bank.json";
-export {default as f_hill} from "./Forest/hill.json";
-export {default as f_lonelyPath} from "./Forest/lonely-path.json";
-export {default as f_bendInThePath} from "./Forest/bend-in-the-path.json";
-export {default as f_abruptTurn} from "./Forest/abrupt-turn.json";
-export {default as f_ruinsEntrance} from "./Forest/ruins-entrance.json";
-export {default as f_quietPath} from "./Forest/quiet-path.json";
-export {default as f_wellWornTrail} from "./Forest/well-worn-trail.json";
-export {default as f_cabinEntrance} from "./Forest/cabin-entrance.json";
-export {default as f_cabinInterior} from "./Forest/cabin-interior.json";
-export {default as f_oldWell} from "./Forest/old-well.json";
-export {default as f_hornetNest} from "./Forest/hornet-nest.json";
-export {default as f_swampEntrance} from "./Forest/swamp-entrance.json";
+export { default as f_brokenBridge } from "./Forest/broken-bridge.json";
+export { default as f_quietFork } from "./Forest/quiet-fork.json";
+export { default as f_westClearing } from "./Forest/west-clearing.json";
+export { default as f_riverBank } from "./Forest/river-bank.json";
+export { default as f_hill } from "./Forest/hill.json";
+export { default as f_lonelyPath } from "./Forest/lonely-path.json";
+export { default as f_bendInThePath } from "./Forest/bend-in-the-path.json";
+export { default as f_abruptTurn } from "./Forest/abrupt-turn.json";
+export { default as f_ruinsEntrance } from "./Forest/ruins-entrance.json";
+export { default as f_quietPath } from "./Forest/quiet-path.json";
+export { default as f_wellWornTrail } from "./Forest/well-worn-trail.json";
+export { default as f_cabinEntrance } from "./Forest/cabin-entrance.json";
+export { default as f_cabinInterior } from "./Forest/cabin-interior.json";
+export { default as f_oldWell } from "./Forest/old-well.json";
+export { default as f_hornetNest } from "./Forest/hornet-nest.json";
+export { default as f_swampEntrance } from "./Forest/swamp-entrance.json";
 
 // Swamp
-export {default as f_forestEntrance} from "./Swamp/forest-entrance.json";
+export { default as s_forestEntrance } from "./Swamp/forest-entrance.json";
+export { default as s_toppledLog } from "./Swamp/toppled-log.json";
+export { default as s_deadTree } from "./Swamp/dead-tree.json";
+export { default as s_standingWater } from "./Swamp/standing-water.json";
+export { default as s_bubblingMuck } from "./Swamp/bubbling-muck.json";
+export { default as s_hut } from "./Swamp/hut.json";
+export { default as s_mammothSkull } from "./Swamp/mammoth-skull.json";
+export { default as s_campsite } from "./Swamp/campsite.json";
+export { default as s_statue } from "./Swamp/statue.json";
+export { default as s_mosquitoSpawn } from "./Swamp/mosquito-spawn.json";
+export { default as s_verticalShaft } from "./Swamp/vertical-shaft.json";
+export { default as s_dryTrail } from "./Swamp/dry-trail.json";
+export { default as s_ridge } from "./Swamp/ridge.json";
 
 // Ruins
-export {default as r_forestEntrance} from "./Ruins/forest-entrance.json";
+export { default as r_forestEntrance } from "./Ruins/forest-entrance.json";
