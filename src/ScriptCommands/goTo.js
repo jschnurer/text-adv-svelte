b0fe525ca6b98321bfb0848dd18bb53ff9592dae
Justin Schnurer <JSchnurer@slb.com>
2020-02-10 11:36:17 -0500

    updates

--- src/ScriptCommands/goTo.js
+++
@@ -1,8 +0,0 @@
-export default function move(dir) {
-  if (!this.room[dir]) {
-    this.write("You can't go that way.");
-    return false;
-  }
-
-  this.parseCmds(this.room[dir]);
-};
--- src/ScriptCommands/ifVar.js
+++ src/ScriptCommands/ifVar.js
@@ -1,6 +1,13 @@
 export default function ifVar(name, operator, compareValue, trueCmds, falseCmds, feature) {
   let value = this.getVarValue(name);
   let checkVal = compareValue;
+
+  if (typeof checkVal === 'number') {
+    if (typeof value === 'undefined') {
+      value = 0;
+    }
+  }
+
   let checkVarName = getVarName(compareValue);
   if (checkVarName) {
     checkVal = this.getVarValue(checkVarName);
@@ -28,7 +35,7 @@ export default function ifVar(name, operator, compareValue, trueCmds, falseCmds,
 function getVarName(compareValue) {
   // Check if it's "{example}". If so,
   // get the value of the "example" variable.
-  let varMatches = compareValue.match(/^\$(.+?)\$$/);
+  let varMatches = compareValue.toString().match(/^\$(.+?)\$$/);
   if (varMatches && varMatches.length === 2) {
     return varMatches[1];
   }
