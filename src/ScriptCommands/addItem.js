6aedeb5fe79f706b2c7f57b94eb40b8955cf7694
Justin Schnurer <justinschnurer@gmail.com>
2020-02-24 17:47:26 -0500

    added count of unique items found throughout the game

--- src/ScriptCommands/addItem.js
+++ src/ScriptCommands/addItem.js
@@ -6,5 +6,13 @@ export default function addItem(id) {
     return;
   }
 
+  // If this is the first time picking up this item,
+  // add it to the overall item count stat.
+  // (Only for non-tutorial items!)
+  if (id.indexOf('TUTORIAL') == -1 && !this.getFlag(`G.ITEM_PICKUP_${id}`)) {
+    this.setFlag(`G.ITEM_PICKUP_${id}`);
+    this.incVar('G.TOTAL_ITEMS_FOUND');
+  }
+
   this.inventory.push(Object.assign({}, item));
 };
