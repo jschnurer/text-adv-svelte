c39c48502a6ea22f28daeea42d0f1989bbe62145
Justin Schnurer <JSchnurer@slb.com>
2020-02-27 16:53:21 -0500

    added save game export/import

--- src/MainMenu.svelte
+++ src/MainMenu.svelte
@@ -1,15 +1,62 @@
 <script>
   import { onMount } from "svelte";
   import { createEventDispatcher } from "svelte";
+  import Modal from "./Modal.svelte";
+
   const dispatch = createEventDispatcher();
   let hasSavedGame = false;
   $: continueDisabled = !hasSavedGame;
+  let exportString = "";
+  let exportTextbox = null;
 
   onMount(() => {
     if (localStorage.getItem("game_state")) {
       hasSavedGame = true;
     }
   });
+
+  const startImport = () => {
+    if (hasSavedGame) {
+      if (
+        !confirm(
+          "Importing a game will erase your current saved game. Continue?"
+        )
+      ) {
+        return;
+      }
+    }
+
+    var gameStr = prompt("Paste your save code here and press OK.", "");
+
+    if (!gameStr) {
+      return;
+    }
+
+    try {
+      let json = atob(gameStr);
+      let gameState = JSON.parse(json);
+      localStorage.setItem("game_state", json);
+      dispatch("continueGame");
+    } catch {
+      alert("Your save code is invalid.");
+    }
+  };
+
+  const startExport = () => {
+    exportString = btoa(localStorage.getItem("game_state"));
+  };
+
+  const copyExport = () => {
+    exportTextbox.select();
+    try {
+      document.execCommand("copy");
+      alert("Copied to clipboard. Paste it somewhere safe!");
+    } catch {
+      alert(
+        "FAiled to copy to clipboard. Select everything in the textbox manually and press CTRL+C instead."
+      );
+    }
+  };
 </script>
 
 <style>
@@ -21,6 +68,17 @@
     color: #999;
     border-style: solid;
     border-color: #666;
+  }
+  .export-text {
+    font-size: 0.5em;
+    min-height: 15em;
+  }
+  .copy {
+    text-decoration: underline;
+    color: #0000ee;
+    cursor: pointer;
+    display: block;
+    margin-top: .5em;
   }
 </style>
 
@@ -34,3 +92,34 @@
   on:click={() => dispatch('continueGame')}>
   Continue Saved Game
 </button>
+<p>
+  If you want to move your saved game onto a different browser, you can export
+  it to text and then import it in a different browser.
+</p>
+<button on:click={startImport}>Import Saved Game ←</button>
+{#if hasSavedGame}
+  <button on:click={startExport}>Export Saved Game →</button>
+{/if}
+{#if exportString}
+  <Modal showClose={true} on:close={() => exportString = ''}>
+    <p>
+      Copy this save code and put it somewhere safe. Then, on another computer,
+      you can import this save by clicking Import Saved Game and pasting this code
+      in.
+    </p>
+    <textarea
+      value={exportString}
+      readonly="readonly"
+      class="export-text"
+      bind:this={exportTextbox} />
+    <span class="copy" on:click={copyExport}>
+      <span
+        style="font-size: .875em; margin-right: .125em; position: relative; top:
+        -.25em; left: -.125em">
+        📄
+        <span style="position: absolute; top: .25em; left: .25em">📄</span>
+      </span>
+      Copy to clipboard
+    </span>
+  </Modal>
+{/if}
--- src/Modal.svelte
+++ src/Modal.svelte
@@ -2,13 +2,24 @@
   import { createEventDispatcher } from "svelte";
   import { fade, fly } from "svelte/transition";
 
-  export let width = undefined;
+  export let width;
+  export let height;
+  export let showClose = false;
 
   const dispatch = createEventDispatcher();
+  let windowWidth = 0;
 
-  function close() {
-    dispatch("close");
-  }
+  $: widthStyle = width ? `width: ${width};` : "";
+  $: heightStyle = height ? `height: ${height};` : "";
+  $: modalStyle = widthStyle + heightStyle;
+
+  const getTransition = () => {
+    if (windowWidth > 500) {
+      return { y: 25, duration: 300 };
+    } else {
+      return { y: 0, duration: 300 };
+    }
+  };
 </script>
 
 <style>
@@ -22,17 +33,18 @@
     opacity: 0.85;
     z-index: 1000;
   }
-
   modal {
     background-color: white;
     display: flex;
     flex-direction: column;
+    overflow-y: auto;
     z-index: 1000;
     background-color: #fff;
     padding: 1em;
     position: fixed;
     border: 1px solid #eee;
     border-radius: 5px;
+    overflow: auto;
     top: 0px;
     left: 0px;
     right: 0px;
@@ -48,7 +60,6 @@
     width: 100%;
     box-sizing: border-box;
   }
-
   @media (min-width: 500px) {
     modal {
       max-height: calc(100% - 40px);
@@ -62,11 +73,33 @@
       transform: perspective(1px) translateY(-50%) translateX(-50%);
     }
   }
+
+  span {
+    position: absolute;
+    top: 0;
+    right: 0.2em;
+    font-variant: small-caps;
+    color: black;
+    cursor: pointer;
+    padding: 0.5em;
+    z-index: 1;
+  }
+
+  span:hover {
+    background-color: rgb(244, 0, 0, 0.2);
+  }
+
+  .showClose {
+    padding-top: 1.5em;
+  }
 </style>
 
+<svelte:window bind:innerWidth={windowWidth} />
+
 <fade transition:fade={{ duration: 200 }} />
-<modal
-  style={width ? 'width:' + width + 'px' : ''}
-  transition:fly={{ y: 25, duration: 300 }}>
+<modal class:showClose style={modalStyle} transition:fly={getTransition()}>
+  {#if showClose}
+    <span on:click={() => dispatch('close')}>x</span>
+  {/if}
   <slot />
 </modal>
