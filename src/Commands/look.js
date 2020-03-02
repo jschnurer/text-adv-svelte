c0b2d35e3b72a01259eec3171d785a4c43cd6fbf
Justin Schnurer <JSchnurer@slb.com>
2020-03-02 12:12:07 -0500

    simplified compass logic

--- src/Commands/look.js
+++ src/Commands/look.js
@@ -5,12 +5,8 @@ export default function look() {
     this.write("^A Very Dark Place^");
     this.write("This place is so dark that you can't even see your hand in front of your face. Strange and terrible things make their homes in the dark. If you value your life, you should head back the way you came IMMEDIATELY and return with a light source.\nIt is pitch black. You are likely to be eaten by a grue.");
   } else {
-    if (this.inventory.find(x => x.id === "COMPASS") && this.room.slug.indexOf('Memories') === -1) {
-      if (this.room.coords) {
-        this.write(`^${this.room.name} (${this.room.coords})^`);
-      } else {
-        this.write(`^${this.room.name}^`);
-      }
+    if (this.inventory.find(x => x.id === "COMPASS") && this.room.coords) {
+      this.write(`^${this.room.name} (${this.room.coords})^`);
     } else {
       this.write(`^${this.room.name}^`);
     }
